4a3924ccaf76365bac5a58c1fdab9f14900da40e
George Kaye <georgejkaye@gmail.com>
2026-03-15 21:32:58 +0000

    feat: add user page map

--- client/src/app/users/[userId]/page.tsx
+++ client/src/app/users/[userId]/page.tsx
@@ -3,10 +3,93 @@ import { UserSummaryContext } from "@/app/context/userSummary"
 import { Loader } from "@/app/components/Loader"
 import { Rating } from "@smastrom/react-rating"
 import Link from "next/link"
-import { useContext } from "react"
-import { SingleUserVisit } from "@/app/api/client"
+import { useContext, useMemo, useState } from "react"
+import { SingleUserVisit, Venue } from "@/app/api/client"
 import { notFound } from "next/navigation"
 import { UserContext } from "@/app/context/user"
+import { GeoJSON, Feature, GeoJsonProperties, Geometry } from "geojson"
+import { VenuesContext } from "@/app/context/venues"
+import bbox from "@turf/bbox"
+import {
+  LngLatBoundsLike,
+  Map,
+  Marker,
+  PaddingOptions,
+  PointLike,
+  Source,
+  ViewState,
+} from "@vis.gl/react-maplibre"
+import Pin from "@/app/components/Pin"
+
+type InitMapViewProps = Partial<ViewState> & {
+  bounds?: LngLatBoundsLike
+  fitBoundsOptions?: {
+    offset?: PointLike
+    minZoom?: number
+    maxZoom?: number
+    padding?: number | PaddingOptions
+  }
+}
+
+interface VenueMapProps {
+  venues: Venue[]
+  userVenueVisitIds: number[]
+}
+
+const VenueMap = ({ venues, userVenueVisitIds }: VenueMapProps) => {
+  const features: Feature[] = venues.map((venue) => ({
+    type: "Feature",
+    properties: {
+      id: venue.venue_id,
+      visited: userVenueVisitIds.includes(venue.venue_id),
+    },
+    geometry: {
+      type: "Point",
+      coordinates: [Number(venue.longitude), Number(venue.latitude)],
+    },
+  }))
+  const featureCollection: GeoJSON<Geometry, GeoJsonProperties> = {
+    type: "FeatureCollection",
+    features,
+  }
+  const [minLng, minLat, maxLng, maxLat] = bbox(featureCollection)
+  const [mapViewState, setMapViewState] = useState<InitMapViewProps>({
+    bounds: [minLng, minLat, maxLng, maxLat],
+    fitBoundsOptions: { padding: 50 },
+  })
+
+  const venuePins = useMemo(
+    () =>
+      venues.map((venue) => (
+        <Marker
+          key={venue.venue_id}
+          longitude={Number(venue.longitude)}
+          latitude={Number(venue.latitude)}
+          anchor="bottom"
+        >
+          <Pin
+            colour={
+              userVenueVisitIds.includes(venue.venue_id) ? "#00a300" : "#960000"
+            }
+            size={30}
+          />
+        </Marker>
+      )),
+    [venues],
+  )
+
+  return (
+    <Map
+      {...mapViewState}
+      style={{ height: "500px" }}
+      mapStyle={"https://tiles.openfreemap.org/styles/bright"}
+    >
+      <Source id="venue" type="geojson" data={featureCollection}>
+        {venuePins}
+      </Source>
+    </Map>
+  )
+}
 
 interface UserSummaryVisitCardProps {
   visit: SingleUserVisit
@@ -60,8 +143,19 @@ const UserSummaryVisitCard = ({ visit }: UserSummaryVisitCardProps) => {
 const Page = () => {
   const { userSummary, isLoadingUserSummary, isError } =
     useContext(UserSummaryContext)
+  const { venues } = useContext(VenuesContext)
+
+  const initialVisitIds: number[] = []
+  const userVenueVisitIds = !userSummary
+    ? []
+    : userSummary.visits.reduce(
+        (acc, cur) =>
+          !acc.includes(cur.venue_id) ? [...acc, cur.venue_id] : acc,
+        initialVisitIds,
+      )
+
   return (
-    <div className="md:w-1/3 flex flex-col items-center md:mx-auto p-4">
+    <div className="md:w-2/3 lg:w-1/3 flex flex-col items-center md:mx-auto p-4">
       {isLoadingUserSummary ? (
         <Loader />
       ) : !userSummary || isError ? (
@@ -73,6 +167,9 @@ const Page = () => {
             {userSummary.visits.length}{" "}
             {userSummary.visits.length === 1 ? "venue" : "venues"} visited
           </div>
+          {venues && venues.length > 0 && (
+            <VenueMap venues={venues} userVenueVisitIds={userVenueVisitIds} />
+          )}
           <div className="flex flex-col gap-4">
             {userSummary.visits
               .sort((a, b) =>
