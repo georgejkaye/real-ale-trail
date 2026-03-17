5bf2c98de9d1e91ee0d95f2e63716ce3d397fb25
George Kaye <georgejkaye@gmail.com>
2026-03-17 20:53:43 +0000

    feat: add pwa stuff

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -31,6 +31,21 @@ export default function RootLayout({
           </UserProvider>
         </ReactQueryClientProvider>
       </body>
+      <link
+        rel="icon"
+        type="image/png"
+        href="/favicon-96x96.png"
+        sizes="96x96"
+      />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <meta name="apple-mobile-web-app-title" content="RealAleTrail" />
+      <link rel="manifest" href="/site.webmanifest" />
     </html>
   )
 }
