a42271336da81030fe0e8b0bd1160ba004ff1ae6
George Kaye <georgejkaye@gmail.com>
2026-03-17 21:38:52 +0000

    feat: move icons around a bit

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -31,19 +31,6 @@ export default function RootLayout({
           </UserProvider>
         </ReactQueryClientProvider>
       </body>
-      <link
-        rel="icon"
-        type="image/png"
-        href="/favicon-96x96.png"
-        sizes="96x96"
-      />
-      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-      <link rel="shortcut icon" href="/favicon.ico" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-touch-icon.png"
-      />
       <meta name="apple-mobile-web-app-title" content="RealAleTrail" />
     </html>
   )
