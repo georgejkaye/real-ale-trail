b11e60f8ca69b5a020759d99063d527c756c2105
George Kaye <georgejkaye@gmail.com>
2026-03-29 16:38:17 +0100

    feat: add crawl support to venue populate script

--- api/src/api/populate.py
+++ api/src/api/populate.py
@@ -1,19 +1,19 @@
 import json
 import sys
+from datetime import datetime
 
 from psycopg import Connection
 
-from api.db.functions.all import insert_venues
+from api.db.functions.all import insert_crawl_fetchone, insert_venues
 from api.db.types.all import VenueInputData
 from api.db.types.register import register_types
 from api.utils import get_secret_file_contents
 
-venue_json_path = sys.argv[1]
+data_path = sys.argv[1]
 
-with open(venue_json_path, "r") as f:
-    venue_json = json.load(f)
+with open(data_path, "r") as f:
+    data = json.load(f)
 
-venues: list[VenueInputData] = []
 
 conn = Connection.connect(
     host=sys.argv[2],
@@ -24,6 +24,26 @@ conn = Connection.connect(
 
 register_types(conn)
 
+crawl_json = data["crawls"]
+venue_json = data["venues"]
+
+venues: list[VenueInputData] = []
+crawl_lookup: dict[str, int] = {}
+
+for crawl in crawl_json:
+    result = insert_crawl_fetchone(
+        conn,
+        crawl["name"],
+        datetime.strptime(crawl["start"], "%Y-%m-%d"),
+        datetime.strptime(crawl["end"], "%Y-%m-%d"),
+        True,
+        crawl["bg"],
+        crawl["fg"],
+    )
+    if result is not None:
+        crawl_lookup[crawl["key"]] = result.crawl_id
+
+
 for venue in venue_json:
     venues.append(
         VenueInputData(
@@ -31,6 +51,7 @@ for venue in venue_json:
             venue["address"],
             venue["latitude"],
             venue["longitude"],
+            [crawl_lookup[key] for key in venue["crawls"]],
         )
     )
 
